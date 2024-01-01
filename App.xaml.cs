19b6782a056ae72da092b52fa2f050fec05ace60
JP <jirankurian@gmail.com>
2024-01-01 23:07:10 +0530

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace kanakuPilla_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace kanakuPilla_maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
